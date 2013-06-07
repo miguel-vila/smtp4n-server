507c1d8f6eeaaa64a3416cc7246502a54260149f
miguel <miguelvilag@gmail.com>
2013-06-07 17:19:24 -0500

    creado metodo de fetchAll e integrado el actor supervisor

--- src/main/scala/co/s4n/smtp/web/Boot.scala
+++ src/main/scala/co/s4n/smtp/web/Boot.scala
@@ -7,6 +7,6 @@ import spray.can.server.SprayCanHttpServerApp
  * Inicializa el servicio
  */
 object Boot extends App with SprayCanHttpServerApp {
-  val service = system.actorOf(Props[MailServiceActor], "Mail-service-actor")
+  val service = system.actorOf(Props[SMTPRestServiceActor], "Mail-service-actor")
   newHttpServer(service) ! Bind(interface = "localhost", port = 8070)  
 }
--- src/main/scala/co/s4n/smtp/web/SMTPRestService.scala
+++ src/main/scala/co/s4n/smtp/web/SMTPRestService.scala
@@ -4,7 +4,7 @@ import akka.actor.{ Actor, ActorSystem, actorRef2Scala, ActorLogging, Props }
 
 import akka.pattern.Patterns
 import akka.util.Timeout
-import co.s4n.smtp.server.actor.{ RequestActor, StatusActor}
+import co.s4n.smtp.server.actor.MailServiceActor
 import co.s4n.smtp.server.message.{ Email, SendRequest, RequestStatus, SuccesfullRequest, FailedRequest, FailedRequestFactory }
 import co.s4n.smtp.server.message.EmailMessageJsonProtocol.emailUnmarshaller
 import co.s4n.smtp.server.message.FailedRequestJsonProtocol.failedRequestUnmarshaller
@@ -18,21 +18,16 @@ import spray.routing.HttpServiceActor
 import scalaz.NonEmptyList
 
 /**
- * MailServiceActor: Actor que hace el ruteo Http
+ * SMTPRestServiceActor: Actor que hace el ruteo Http
  */
-class MailServiceActor extends Actor with HttpServiceActor {
+class SMTPRestServiceActor extends Actor with HttpServiceActor {
 
   def receive = runRoute(mailServiceRoute)
 
   /**
-   * Actor que atiende las solicitudes de envío de correo
+   * Actor que unifica la lógica del servidor
    */
-  val requestActor = actorRefFactory.actorOf(Props[RequestActor], "RequestActor")
-
-  /**
-   * Actor que maneja la persistencia de los estados de solicitud
-   */
-  val statusActor = actorRefFactory.actorOf(Props[StatusActor], "StatusActor")
+  val mailServiceActor = actorRefFactory.actorOf(Props[MailServiceActor], "RequestActor")
 
   /**
    * Ruteo
@@ -48,7 +43,7 @@ class MailServiceActor extends Actor with HttpServiceActor {
         entity(as[Email]) { email =>
           println("Request received: " + email.toString)
           val ticket = System.currentTimeMillis
-          requestActor ! SendRequest(email.toEmailVO, ticket)
+          mailServiceActor ! SendRequest(email.toEmailVO, ticket)
           println("Sending RequestTicket: " + ticket.toString)
           complete(Accepted, ticket.toString)
         }
@@ -64,7 +59,7 @@ class MailServiceActor extends Actor with HttpServiceActor {
           try {
             val requestTicket = requestTicketString.toLong
             val timeout = Timeout(Duration.create(5, "seconds"))
-            val validationFuture = Patterns.ask(statusActor, requestTicket, timeout).mapTo[Validation[NonEmptyList[Throwable], Option[RequestStatus]]]
+            val validationFuture = Patterns.ask(mailServiceActor, requestTicket, timeout).mapTo[Validation[NonEmptyList[Throwable], Option[RequestStatus]]]
             val validation = Await.result(validationFuture, Duration.create(5, "seconds"))
             validation.fold(
               fail => complete(InternalServerError, FailedRequestFactory(requestTicket, fail)),
@@ -74,7 +69,7 @@ class MailServiceActor extends Actor with HttpServiceActor {
                 case None => complete(NotFound)
               })
           } catch {
-            case _: NumberFormatException => complete(BadRequest)
+            case _: NumberFormatException => complete(BadRequest, "Formato de ticket inválido")
           }
         }
       }
